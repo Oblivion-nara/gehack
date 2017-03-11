1c0d72515c3762be50eb28f787bc20341543f7f7
Reece M <reecemeek@gmail.com>
2017-03-11 03:08:58 +0000

    Added login functions with direct login

--- src/server/OBPInterface.py
+++ src/server/OBPInterface.py
@@ -5,6 +5,8 @@ class OBPInterface:
     def __init__(self):
         self.bankID = -1
         self.accountID = -1
+        self.token = -1
+
 
     def getBankID(self):
         return self.bankID
@@ -18,13 +20,23 @@ class OBPInterface:
     def setAccountID(self, accountID):
         self.accountID = accountID
 
+    def postRequest(self, uri, data):
+        r = request.post("https://apisandbox.openbankproject.com/obp/v2.2.0" + uri, data})
+
+        return r.json()
+
     def getRequest(self, uri):
-        r = request.get("https://apisandbox.openbankproject.com/obp/v2.2.0" + uri)
+        r = request.get("https://apisandbox.openbankproject.com/obp/v2.2.0" + uri, data = {"Autherization" : "DirectLogin token=\""+self.token+"\""})
         r.status_code
         r.headers['content-type']
         r.encoding
         r.text
         return r.json()
+
+    def login(self, username, password):
+        result = postRequest("/my/logins/direct", data = {"Authorization":"DirectLogin username=\""+username+"\","
+                        + "password=\""+password+"\", consumer_key=\"hznak4pyka33m2pn2yq5gr5le0x2itnnsbyht4jp\"","Content-Type": "application/json" })
+        self.token = result["token"]
 
     def root(self):
         return getRequest("/root")
@@ -33,10 +45,16 @@ class OBPInterface:
         return getRequest("/banks")
 
     def getBankDetails(self):
-        return getRequest("/banks/" + bankID)
+        if(self.bankID == -1):
+            return None
+        return getRequest("/banks/" + self.bankID)
 
     def getAccountDetails(self):
-        return getRequest("/my/banks/" + self.bankID + "/accounts/" + accountID + "/account")
+        if(self.accountID == -1 or self.bankID == -1):
+            return None
+        return getRequest("/my/banks/" + self.bankID + "/accounts/" + self.accountID + "/account")
 
     def getTransactions(self):
-        getRequest("/my/" + self.bankID + "/rbs/accounts/" + self.accountID + "/transactions")
+        if(self.accountID == -1 or self.bankID == -1):
+            return None
+        return getRequest("/my/" + self.bankID + "/rbs/accounts/" + self.accountID + "/transactions")
