880fd2bd70a5b112baa937ad4d7407eb17d78863
peran <perantruscott@gmail.com>
2017-03-11 09:44:50 +0000

    Large commit.  Fixed #26, #24, and #31 along with overhauling the graphs and fixing a lot of bugs

--- src/client/public/main.js
+++ src/client/public/main.js
@@ -61,36 +61,68 @@ getAjax("/transactions", function(data) {
 
 getAjax("/transactionArrays", function(data) {
     data = JSON.parse(data);
-    balance = data.balances;
-    myChart.data.datasets[0].data = balance;
-    var oldDate = null;
+    data.balances.reverse();
+    data.dates.reverse();
+    data.changes.reverse();
+    data.names.reverse();
+
+    var thisMonth = data.dates[data.dates.length-1].substr(5, 2);
+
+    var dates = [];
+
+    var incomeLastMonth = 0;
+    var outcomeLastMonth = 0;
+
+    for (var i = 0; i < data.dates.length; i++) {
+        dates.push(data.dates[i].substr(0, 10));
+    }
+    var oldDate = "";
     var desc = "";
     for (var i = 0; i < data.dates.length; i++) {
         if (i==0) {
-            balanceDif.push(data.changes[i]);
+            balance.push(data.balances[i]);
+            balanceDif.push(parseFloat(data.changes[0]));
+            console.log(balanceDif);
             desc+=getDesc(data.names[i], data.changes[i]);
-            oldDate = data.dates[i];
+            oldDate = dates[i];
         }
-        else if (oldDate == data.dates[i] && i != data.dates.length-1) {
-            balanceDif[i] += data.changes[i];
-            desc+=getDesc(data.names[i], data.changes[i]);
+        else if (oldDate == dates[i] && i != data.dates.length-1) {
+            balanceDif[balanceDif.length - 1] += parseFloat(data.changes[i]);
+            desc += getDesc(data.names[i], data.changes[i]);
         }
         else {
-            balanceDif.push(data.changes[i]);
-            difDataPoints.push(new DataPoint(desc, balanceDif[i-1], data.dates[i-1]));
+            balanceDif.push(parseFloat(data.changes[i]));
+            difDataPoints.push(new DataPoint(desc, balanceDif[balanceDif.length-2].toFixed(2), dates[i-1]));
             desc = getDesc(data.names[i], data.changes[i]);
-            oldDate = data.dates[i];
+            oldDate = dates[i];
+            balance.push(data.balances[i]);
         }
         if (i == data.dates.length-1) {
-            difDataPoints.push(new DataPoint(desc, balanceDif[i], data.dates[i]));
+            difDataPoints.push(new DataPoint(desc, balanceDif[balanceDif.length-1].toFixed(2), dates[i]));
+        }
+        if (dates[i].substr(5,2) === thisMonth) {
+            if (parseFloat(data.changes[i]) > 0) incomeLastMonth += parseFloat(data.changes[i]);
+            else outcomeLastMonth += parseFloat(data.changes[i]);
+        }
+        else {
+            console.log(dates[i].substr(5,2) + " " + thisMonth);
         }
     }
 
+    //console.log(balanceDif);
 
+    myChart.data.datasets[0].data = balance;
     myChart.data.labels = difDataPoints;
     myChart1.data.labels = difDataPoints;
     myChart.update();
     myChart1.update();
+    var bString = "Balance: ";
+    if (balance[balance.length-1] < 0) bString += "-";
+    bString += "£"+balance[balance.length-1];
+    document.getElementById('total').innerHTML = bString+".  Transactions in the last month:";
+    document.getElementById('up').innerHTML = incomeLastMonth.toString();
+    document.getElementById('down').innerHTML = outcomeLastMonth.toString();
+
 });
 
 function getDesc(name, value) {
