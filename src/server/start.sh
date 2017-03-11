7884d0096d865189fda155758a1eb126091ce8b8
TauOmicronMu <TauOmicronMuRS@Gmail.com>
2017-03-11 01:52:57 +0000

    run command in background when running start

--- src/server/start.sh
+++ src/server/start.sh
@@ -5,4 +5,4 @@ clear
 echo "Gunicorn : Starting flask server on port 80 with 5 workers, ."
 
 cd /root/gehack/src/server
-gunicorn --workers 5 --timeout 2147483647 --bind 0.0.0.0:80 wsgi:app 
+gunicorn --workers 5 --timeout 2147483647 --bind 0.0.0.0:80 wsgi:app &>/dev/null &  
