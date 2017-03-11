0632d800ecd8e539de2be215596165bf6ec79eaf
Reece Bennett <reece.bennett1@gmail.com>
2017-03-11 04:12:08 +0000

    Move static files to src/client/public

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,22 @@
 var express = require("express");
 var app = express();
+var path = require("path");
 
+// Serve static files
+app.use(express.static("src/client/public"));
+
+// Directory of client stuff
+var client = path.join(__dirname, "../client/")
+
+// Serve the homepage
 app.get("/", function(req, res) {
-  res.send("Hello world!");
+  res.sendFile(path.join(client, "index.html"));
+})
+
+// Listen for logins
+app.get("/login", function(req, res) {
+  console.log("User " + req.query.user + " with pass " + req.query.pass);
+  res.redirect("/?user=" + req.query.user + "&pass=" + req.query.pass);
 });
 
 app.listen(3000, function() {
